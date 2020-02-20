dba8ed61973ce91b05d3e5c8ee122d8832828f18
YarikMix <43493024+Test9136@users.noreply.github.com>
2020-02-20 18:25:39 +0300

    qqqqq

--- first-menu/Begin-menu/Begin-1/js/script.js
+++ first-menu/Begin-menu/Begin-1/js/script.js
@@ -2,10 +2,20 @@ function getRandomInt(min, max){
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-var a = getRandomInt(1, 10);
-var P = 4*a;
-
 $(document).ready(function() {
+
+	a = getRandomInt(1, 10);
+
+	while(true){
+		if(a != parseInt($(".example:nth-child(2) .first-part").text().slice(3))){
+			break;
+		}
+
+		a = getRandomInt(1, 10);
+	}
+
+	var P = 4*a;
+
 	$(".example:last-child .first-part").html("a = " + a);
 	$(".example:last-child .last-part").html("P = " + P);
 });
