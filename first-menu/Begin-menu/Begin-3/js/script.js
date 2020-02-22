347e8b6a2875b138bdb4ed80bf284c6de5547243
YarikMix <43493024+Test9136@users.noreply.github.com>
2020-02-22 18:12:08 +0300

    aaafffaaa

--- first-menu/Begin-menu/Begin-3/js/script.js
+++ first-menu/Begin-menu/Begin-3/js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 	b = getRandomInt(1,10);
 
 	while(true){
-		if(a != parseInt($(".example:nth-child(2) .first-part").text().slice(3,4)) &&
+		if(a != parseInt($(".example:nth-child(2) .first-part").text().slice(5,6)) &&
 		 b != parseInt($(".example:nth-child(2) .first-part").text().slice(10))){
 			break;
 		}
