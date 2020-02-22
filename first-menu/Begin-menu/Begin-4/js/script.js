292784e86280845245b5c778868654ec363f763a
YarikMix <43493024+Test9136@users.noreply.github.com>
2020-02-22 18:51:39 +0300

    asdasdfadsf

--- first-menu/Begin-menu/Begin-4/js/script.js
+++ first-menu/Begin-menu/Begin-4/js/script.js
@@ -4,18 +4,19 @@ function getRandomInt(min, max){
 
 $(document).ready(function() {
 
-	a = getRandomInt(1, 10);
+	d = getRandomInt(1, 10);
 
 	while(true){
-		if(a != parseInt($(".example:nth-child(2) .first-part").text().slice(3))){
+		if(d != 2){
 			break;
 		}
 
-		a = getRandomInt(1, 10);
+		d = getRandomInt(1, 10);
 	}
 
-	var P = 4*a;
-
-	$(".example:last-child .first-part").html("a = " + a);
-	$(".example:last-child .last-part").html("P = " + P);
+	const pi = 3.14;
+	var L = pi*d;				
+					
+	$(".example:last-child .first-part").html("d = " + d);
+	$(".example:last-child .last-part").html("L = " + L);
 });
