9f0b8f491c7aa1461414b34d72cc31f8bad088cb
YarikMix <43493024+Test9136@users.noreply.github.com>
2020-02-22 18:27:30 +0300

    afafafafafaa

--- first-menu/Begin-menu/Begin-3/js/script.js
+++ first-menu/Begin-menu/Begin-3/js/script.js
@@ -4,6 +4,22 @@ function getRandomInt(min, max){
 
 $(document).ready(function() {
 
-	alert(parseInt($(".example:nth-child(2) .first-part").text().slice(5,6)));
-	alert(parseInt($(".example:nth-child(2) .first-part").text().slice(10)));
+	a = getRandomInt(1, 10);
+	b = getRandomInt(1,10);
+
+	while(true){
+		if(a != parseInt($(".example:nth-child(2) .first-part").text().slice(5,6)) &&
+		 b != parseInt($(".example:nth-child(2) .first-part").text().slice(10))){
+			break;
+		}
+
+		a = getRandomInt(1, 10);
+		b = getRamdomInt(1, 10);
+	}
+
+	var S = a*b;
+	var P = 2*(a+b);
+
+	$(".example:last-child .first-part").html("a = " + a + "<br> b = " + b);
+	$(".example:last-child .last-part").html("S = " + S + "<br> P  = " + P);
 });
