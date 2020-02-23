d62b164a3daccb46332820788e6496e3a9c48192
YarikMix <43493024+Test9136@users.noreply.github.com>
2020-02-23 17:32:30 +0300

    ggaagg

--- first-menu/Begin-menu/Begin-5/js/script.js
+++ first-menu/Begin-menu/Begin-5/js/script.js
@@ -3,24 +3,23 @@ $(document).ready(function() {
   		return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
 	a = getRandomInt(1, 10);
-	b = getRandomInt(1,10);
 
 	while(true){
 		
-		if(a != 2 && b != 3){
+		if(a != 2){
 			break;
 		}
 
 		a = getRandomInt(1, 10);
-		b = getRandomInt(1, 10);
 		
 	}
 
-	var S = a*b;
-	var P = 2*(a+b);
+	var V = Math.round(Math.pow(a,3) * 1000) / 1000;
+	var S = Math.round(6*Math.pow(a,2) * 10) / 10;
+	alert("V = " + V + "        S = " + S);
 
-	$(".example:last-child .first-part").html("a = " + a + "<br> b = " + b);
-	$(".example:last-child .last-part").html("S = " + S + "<br> P  = " + P);
+	$(".example:last-child .first-part").html("a = " + a);
+	$(".example:last-child .last-part").html("V = " + V + "<br> s  = " + S);
 });
 
 	
